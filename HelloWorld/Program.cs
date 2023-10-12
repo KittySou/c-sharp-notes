217dd819fcd60b343a4eea107ba5704ed56fe736
kitty <kitty.souriyadeth@hotmail.fr>
2023-10-12 12:05:57 -0400

    data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string stringDataType = "Hello, World!";
+Console.WriteLine(stringDataType);
 
 int intDataType = 1;
 Console.WriteLine(intDataType);
+
+double doubleDataType = 239.43909;
+Console.WriteLine(doubleDataType);
+
+char charDataType = 'a';
+Console.WriteLine(charDataType);
+// make sure to use single quotes when using char,
+// double quotes "" are for strings
+
+bool boolDataType = true;
+Console.WriteLine(boolDataType);
